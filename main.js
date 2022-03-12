1a046c03bb52bade0969d72079b43435442114cb
Jahnavi1203 <87400772+Jahnavi1203@users.noreply.github.com>
2022-03-11 20:22:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    clownNose = loadImage('https://i.postimg.cc/zB9r7Mjn/clown-Nose.png');
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x -15;
+        noseY = results[0].pose.nose.y -15;
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+    image(clownNose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
